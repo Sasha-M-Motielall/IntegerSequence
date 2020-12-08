e8a5988723405a1c3963034325686d2ca10eac30
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-08 13:00:58 -0500

    ArraySequence method created

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,6 +1,14 @@
 import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
-  private int []data;
+  private int[] data;
+
+  public ArraySequence(int[] other) {
+    this.currentIndex = 0;
+    this.data = new int[other.length];
+    for (int i = 0; i < data.length; i++) {
+      data[i] = other[i];
+    }
+  }
 
 }
